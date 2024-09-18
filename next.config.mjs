d3b3d812cb47b5b5156e465f8f69f3440a518e4d
ddolboghi <rhdfyd128@gmail.com>
2024-09-18 23:44:37 +0900

    chore: 기본 페이지 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/sale",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;

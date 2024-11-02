3e6b92c676953501ebf118b5511a9a916279d884
ddolboghi <rhdfyd128@gmail.com>
2024-11-02 19:39:13 +0900

    refactor: 보고 함수 라이브러리화 & useMemo 적용

--- components/sale/Result.tsx
+++ components/sale/Result.tsx
@@ -1,19 +1,36 @@
 "use client";
-
-import { translateToKoreanDayOfWeek } from "@/libs/dates";
-import { sumPercentages, sumTableNum } from "@/libs/sale";
+import { getReportDate } from "@/libs/dates";
+import { getReport } from "@/libs/reports";
+import {
+  getAdditionalOrderSums,
+  getOrderSums,
+  sumPercentages,
+  sumTableNum,
+} from "@/libs/sale";
+import { AdditionalOrders, Drink, Orders, Percentages } from "@/utils/types";
+import { useMemo } from "react";
 
 type ResultProps = {
-  drink: { [key: string]: { [key: number]: number } };
-  percentages: { [key: string]: { [key: number]: number } };
+  drink: Drink;
+  percentages: Percentages;
   totalBisness: number;
-  orders: {
-    [key: number]: { [key: string]: number | string };
-  };
-  additionalOrders: {
-    [key: number]: { [key: string]: number | string };
-  };
+  orders: Orders;
+  additionalOrders: AdditionalOrders;
 };
+
+const calculateTotals = (drink: Drink) => ({
+  muhak: sumTableNum(drink["Muhak"]),
+  hite: sumTableNum(drink["Hite"]),
+  daesun: sumTableNum(drink["Daesun"]),
+  lotte: sumTableNum(drink["Lotte"]),
+});
+
+const calculatePercentages = (percentages: Percentages) => ({
+  muhak: sumPercentages(percentages["Muhak"]),
+  hite: sumPercentages(percentages["Hite"]),
+  daesun: sumPercentages(percentages["Daesun"]),
+  lotte: sumPercentages(percentages["Lotte"]),
+});
 
 export default function Result({
   drink,
@@ -22,174 +39,63 @@ export default function Result({
   orders,
   additionalOrders,
 }: ResultProps) {
-  const kstDate = new Date(
-    new Date().toLocaleString("en-US", { timeZone: "Asia/Seoul" })
+  const brandTotals = useMemo(() => calculateTotals(drink), [drink]);
+  const brandPercentages = useMemo(
+    () => calculatePercentages(percentages),
+    [percentages]
   );
-  const month = String(kstDate.getMonth() + 1).padStart(2, "");
-  const day = String(kstDate.getDate()).padStart(2, "0");
-  const dayOfWeek = translateToKoreanDayOfWeek(kstDate.getDay());
 
-  const muhakTotal = sumTableNum(drink["Muhak"]);
-  const hiteTotal = sumTableNum(drink["Hite"]);
-  const daesunTotal = sumTableNum(drink["Daesun"]);
-  const lotteTotal = sumTableNum(drink["Lotte"]);
-  const totalTableNum = muhakTotal + hiteTotal + daesunTotal + lotteTotal;
+  const workerNames = useMemo(
+    () =>
+      Object.values(orders)
+        .map((order) => order.name)
+        .join(", "),
+    [orders]
+  );
 
-  const muhakPer = sumPercentages(percentages["Muhak"]);
-  const hitePer = sumPercentages(percentages["Hite"]);
-  const daesunPer = sumPercentages(percentages["Daesun"]);
-  const lottePer = sumPercentages(percentages["Lotte"]);
+  const orderSums = useMemo(() => getOrderSums(orders), [orders]);
+  const additionalOrderSums = useMemo(
+    () => getAdditionalOrderSums(additionalOrders),
+    [additionalOrders]
+  );
 
-  const workers = new Set<string>([]);
+  const BSKYReport = useMemo(
+    () =>
+      getReport(
+        false,
+        drink,
+        percentages,
+        totalBisness,
+        orders,
+        additionalOrders
+      ),
+    [drink, percentages, totalBisness, orders, additionalOrders]
+  );
 
-  let workerReportOfOrders = "";
-  for (const order of Object.values(orders)) {
-    workers.add(order["name"] as string);
-    workerReportOfOrders += `${order["name"]}: ${
-      order["goodDay"] || ""
-    }t(좋은데이) / ${order["toctoc"] || ""}t(톡소다) / ${
-      order["galmegi"] || ""
-    }t(부산갈매기)\n`;
-  }
-
-  const workersString = Array.from(workers).join(", ");
-
-  let workerReportOfAdditionalOrders = "";
-  for (const order of Object.values(additionalOrders)) {
-    workerReportOfAdditionalOrders += `${order["name"]}: ${
-      order["goodDay"] || ""
-    }t(좋은데이) / ${order["toctoc"] || ""}t(톡소다) / ${
-      order["galmegi"] || ""
-    }t(부산갈매기)\n`;
-  }
-
-  const orderSums = { 1: 0, 2: 0, 3: 0 };
-  for (const order of Object.values(orders)) {
-    orderSums[1] += Number(order["goodDay"]) || 0;
-    orderSums[2] += Number(order["toctoc"]) || 0;
-    orderSums[3] += Number(order["galmegi"]) || 0;
-  }
-
-  const additionalOrderSums = { 1: 0, 2: 0, 3: 0 };
-  for (const addi of Object.values(additionalOrders)) {
-    additionalOrderSums[1] += Number(addi["goodDay"]) || 0;
-    additionalOrderSums[2] += Number(addi["toctoc"]) || 0;
-    additionalOrderSums[3] += Number(addi["galmegi"]) || 0;
-  }
-  console.log(percentages);
-  const report = `<${month}월 ${day}일 ${dayOfWeek} 광안 상권보고>
-1. 점유비
-\u0020\u0020- 총 방문업소: ${totalBisness}개
-\u0020\u0020- 총 테이블 수: ${totalTableNum}t
-가. 무학 : ${muhakTotal}t (${muhakPer}%)
-\u0020\u0020- 좋은데이: ${drink["Muhak"][1] || " "}t (${
-    percentages["Muhak"][1] ? percentages["Muhak"][1].toFixed(1) : " "
-  }%)
-\u0020\u0020- 톡시리즈 : ${drink["Muhak"][2] || " "}t (${
-    percentages["Muhak"][2] ? percentages["Muhak"][2].toFixed(1) : " "
-  }%)
-\u0020\u0020- 부산갈매기 : ${drink["Muhak"][3] || " "}t (${
-    percentages["Muhak"][3] ? percentages["Muhak"][3].toFixed(1) : " "
-  }%)
-나. 하이트진로 : ${hiteTotal}t (${hitePer}%)
-\u0020\u0020- 참이슬 : ${drink["Hite"][1] || " "}t (${
-    percentages["Hite"][1] ? percentages["Hite"][1].toFixed(1) : " "
-  }%)
-\u0020\u0020- 진로 : ${drink["Hite"][2] || " "}t (${
-    percentages["Hite"][2] ? percentages["Hite"][2].toFixed(1) : " "
-  }%)
-\u0020\u0020- 기타(진로골드) : ${drink["Hite"][3] || " "}t (${
-    percentages["Hite"][3] ? percentages["Hite"][3].toFixed(1) : " "
-  }%)
-다. 대선주조 : ${daesunTotal}t (${daesunPer}%)
-\u0020\u0020- 대선(C1포함) : ${drink["Daesun"][1] || " "}t (${
-    percentages["Daesun"][1] ? percentages["Daesun"][1].toFixed(1) : " "
-  }%)
-\u0020\u0020- 강알리 : ${drink["Daesun"][2] || " "}t (${
-    percentages["Daesun"][2] ? percentages["Daesun"][2].toFixed(1) : " "
-  }%)
-\u0020\u0020- 기타 : ${drink["Daesun"][3] || " "}t (${
-    percentages["Daesun"][3] ? percentages["Daesun"][3].toFixed(1) : " "
-  }%)
-라. 롯데 : ${lotteTotal}t (${lottePer}%)
-\u0020\u0020- 새로: ${drink["Lotte"][1] || " "}t (${
-    percentages["Lotte"][1] ? percentages["Lotte"][1].toFixed(1) : " "
-  }%)
-\u0020\u0020- 새로(살구) : ${drink["Lotte"][2] || " "}t (${
-    percentages["Lotte"][2] ? percentages["Lotte"][2].toFixed(1) : " "
-  }%)
-\u0020\u0020- 청하(별빛청하 포함) : ${drink["Lotte"][3] || " "}t (${
-    percentages["Lotte"][3] ? percentages["Lotte"][3].toFixed(1) : " "
-  }%)
-마. 기타 0t (0%)\n
-2. 전환 및 추가주문
-가. 근무인원
-${workersString}
-나. 전환: ${orderSums[1] || 0}t(좋은데이) / ${orderSums[2] || 0}t(톡소다) / ${
-    orderSums[3] || 0
-  }t(부산갈매기)
-${workerReportOfOrders}
-다. 추가주문: ${additionalOrderSums[1] || 0}t(좋은데이) / ${
-    additionalOrderSums[2] || 0
-  }t(톡소다) / ${additionalOrderSums[3] || 0}t(부산갈매기) 
-${workerReportOfAdditionalOrders}
-`;
-
-  const smReport = `<이순조SM 퇴근보고>
-1. 야간판촉지역
-광안 바닷가
-2. 야간 음용비
-좋은데이 : ${drink["Muhak"][1] || " "}T - ${
-    percentages["Muhak"][1] ? percentages["Muhak"][1].toFixed(1) : " "
-  }%
-좋은데이 톡시리즈 : ${drink["Muhak"][2] || " "}T - ${
-    percentages["Muhak"][2] ? percentages["Muhak"][2].toFixed(1) : " "
-  }%
-갈매기 : ${drink["Muhak"][3] || " "}T - ${
-    percentages["Muhak"][3] ? percentages["Muhak"][3].toFixed(1) : " "
-  }%
-대선 : ${drink["Daesun"][1] || " "}T - ${
-    percentages["Daesun"][1] ? percentages["Daesun"][1].toFixed(1) : " "
-  }% 
-강알리 : ${drink["Daesun"][2] || " "}T - ${
-    percentages["Daesun"][2] ? percentages["Daesun"][2].toFixed(1) : " "
-  }%
-진로 : ${drink["Hite"][2] || " "}T - ${
-    percentages["Hite"][2] ? percentages["Hite"][2].toFixed(1) : " "
-  }%
-진로(골드) : ${drink["Hite"][3] || " "}T - ${
-    percentages["Hite"][3] ? percentages["Hite"][3].toFixed(1) : " "
-  }%
-참이슬 : ${drink["Hite"][1] || " "}T - ${
-    percentages["Hite"][1] ? percentages["Hite"][1].toFixed(1) : " "
-  }%
-C1: T - %
-기타
-새로: ${drink["Lotte"][1] || " "}T - ${
-    percentages["Lotte"][1] ? percentages["Lotte"][1].toFixed(1) : " "
-  }%
-새로(살구): ${drink["Lotte"][2] || " "}T - ${
-    percentages["Lotte"][2] ? percentages["Lotte"][2].toFixed(1) : " "
-  }%
-청하: ${drink["Lotte"][3] || " "}T - ${
-    percentages["Lotte"][3] ? percentages["Lotte"][3].toFixed(1) : " "
-  }%
-
-갈매기 드시던 테이블까지 ${
-    drink["Muhak"][3] || 0 + orderSums[3] + additionalOrderSums[3]
-  }개입니다.`;
-  const handleAllWriteClipboard = () => {
+  const smReport = useMemo(
+    () =>
+      getReport(
+        true,
+        drink,
+        percentages,
+        totalBisness,
+        orders,
+        additionalOrders
+      ),
+    [drink, percentages, totalBisness, orders, additionalOrders]
+  );
+  const handleBSKYReportClipboard = () => {
     if (
       typeof window !== "undefined" &&
       typeof window.navigator !== "undefined"
     ) {
-      navigator.clipboard.writeText(report).then(() => {
+      navigator.clipboard.writeText(BSKYReport).then(() => {
         alert("클립보드에 복사했습니다.");
       });
     }
   };
 
-  const handleSMWriteClipboard = () => {
+  const handleSMReportClipboard = () => {
     if (
       typeof window !== "undefined" &&
       typeof window.navigator !== "undefined"
@@ -203,16 +109,16 @@ C1: T - %
   return (
     <div>
       <div className="border border-blue-300">
-        <h1>
-          {"<"}
-          {month}월 {day}일 {dayOfWeek} 광안 상권 보고{">"}
-        </h1>
+        <h1>{getReportDate()}</h1>
         <h2>1. 점유비</h2>
         <p>&nbsp;&nbsp;- 총 방문업소: {totalBisness}</p>
-        <p>&nbsp;&nbsp;- 총 테이블 수: {totalTableNum}t</p>
+        <p>
+          &nbsp;&nbsp;- 총 테이블 수:{" "}
+          {Object.values(brandTotals).reduce((acc, cur) => acc + cur, 0)}t
+        </p>
         <section>
           <h3>
-            가. 무학: {muhakTotal}t ({muhakPer}%)
+            가. 무학: {brandTotals.muhak}t ({brandPercentages.muhak}%)
           </h3>
           <p>
             &nbsp;&nbsp;- 좋은데이: {drink["Muhak"][1] || 0}t (
@@ -231,7 +137,7 @@ C1: T - %
         </section>
         <section>
           <h3>
-            나. 하이트진로: {hiteTotal}t ({hitePer}%)
+            나. 하이트진로: {brandTotals.hite}t ({brandPercentages.hite}%)
           </h3>
           <p>
             &nbsp;&nbsp;- 참이슬: {drink["Hite"][1] || 0}t (
@@ -248,7 +154,7 @@ C1: T - %
         </section>
         <section>
           <h3>
-            다. 대선주조: {daesunTotal}t ({daesunPer}%)
+            다. 대선주조: {brandTotals.daesun}t ({brandPercentages.daesun}%)
           </h3>
           <p>
             &nbsp;&nbsp;- 대선(C1포함): {drink["Daesun"][1] || 0}t (
@@ -268,7 +174,7 @@ C1: T - %
         </section>
         <section>
           <h3>
-            라. 롯데: {lotteTotal}t ({lottePer}%)
+            라. 롯데: {brandTotals.lotte}t ({brandPercentages.lotte}%)
           </h3>
           <p>
             &nbsp;&nbsp;- 새로: {drink["Lotte"][1] || 0}t (
@@ -290,7 +196,7 @@ C1: T - %
         <h2>2. 전환 및 추가주문</h2>
         <section>
           <h3>가. 근무인원</h3>
-          <p>{workersString}</p>
+          <p>{workerNames}</p>
         </section>
         <section>
           <h3>
@@ -392,20 +298,20 @@ C1: T - %
           <br />
           <p>
             갈매기 드시던 테이블까지{" "}
-            {drink["Muhak"][3] || 0 + orderSums[3] + additionalOrderSums[3]}
+            {(drink["Muhak"][3] || 0) + orderSums[3] + additionalOrderSums[3]}
             개입니다.
           </p>
         </section>
       </div>
       <button
         className="my-2 bg-blue-500 text-white rounded p-2 w-full"
-        onClick={handleAllWriteClipboard}
+        onClick={handleBSKYReportClipboard}
       >
         수영구 톡방용 보고 복사하기
       </button>
       <button
         className="my-2 bg-green-400 text-white rounded p-2 w-full"
-        onClick={handleSMWriteClipboard}
+        onClick={handleSMReportClipboard}
       >
         담당자님용 보고 복사하기
       </button>

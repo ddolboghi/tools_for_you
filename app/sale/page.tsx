4380f7f872c42a7a341dd411efe86c84799fc6c2
ddolboghi <rhdfyd128@gmail.com>
2024-11-15 16:47:04 +0900

    feat: footer 추가

--- app/sale/page.tsx
+++ app/sale/page.tsx
@@ -4,6 +4,9 @@ export default function page() {
   return (
     <main>
       <SaleCalculation />
+      <footer className="text-center text-sm text-[#9ca1a7] py-2 bg-[#F6F8FA]">
+        문의: jhasd128@gmail.com
+      </footer>
     </main>
   );
 }

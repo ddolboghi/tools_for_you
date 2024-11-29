2b96d8d766cebb42891685c49a1943efbbb1363d
ddolboghi <rhdfyd128@gmail.com>
2024-11-30 00:28:17 +0900

    refactor & feat: 주문 합계 로직 수정 & 초기화 함수 추가

--- libs/sale/sale.ts
+++ libs/sale/sale.ts
@@ -78,16 +78,44 @@ export const calculateAdjustedPercentages = (
 
 export const getOrderSums = (orders: Orders) => {
   const orderSums: OrderSums = {};
-  console.log(orders);
   const keys = Object.keys(Object.values(orders)[0] || {});
-  keys.forEach((_, index) => {
-    orderSums[index] = 0;
+  keys.forEach((key) => {
+    if (Number(key) > 0) orderSums[Number(key)] = 0;
   });
+
   for (const order of Object.values(orders)) {
-    Object.values(order).forEach((value, index) => {
-      orderSums[index] += Number(value) || 0;
+    Object.entries(order).forEach(([key, value]) => {
+      if (key !== "0") {
+        orderSums[Number(key)] += Number(value) || 0;
+      }
     });
   }
 
+  return orderSums;
+};
+
+export const getInitOrder = (onSplit: boolean, orders: Orders): Orders => {
+  if (onSplit) {
+    Object.keys(orders).forEach((key) => {
+      orders[Number(key)][3] = 0;
+      orders[Number(key)][4] = 0;
+    });
+    return orders;
+  }
+  Object.keys(orders).forEach((key) => {
+    orders[Number(key)][3] = 0;
+    delete orders[Number(key)][4];
+  });
+  return orders;
+};
+
+export const getInitOrderSums = (onSplit: boolean, orderSums: OrderSums) => {
+  if (onSplit) {
+    orderSums[3] = 0;
+    orderSums[4] = 0;
+    return orderSums;
+  }
+  orderSums[3] = 0;
+  delete orderSums[4];
   return orderSums;
 };

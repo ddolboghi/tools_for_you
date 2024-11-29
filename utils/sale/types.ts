235729a2254056964b2f98d0df5d924ee86b25aa
ddolboghi <rhdfyd128@gmail.com>
2024-11-29 17:07:35 +0900

    feat: 타입 추가

--- utils/sale/types.ts
+++ utils/sale/types.ts
@@ -6,16 +6,70 @@ export type Percentages = {
   [key: string]: { [key: number]: number };
 };
 
+type OrderItem = {
+  name: string;
+  goodDay: number;
+  toc: number;
+} & (
+  | { galmegi: number; galmegi19?: never; galmegi16?: never }
+  | { galmegi?: never; galmegi19: number; galmegi16: number }
+);
+
 type OrdersDataType = {
-  [key: number]: { [key: string]: number | string };
+  [key: number]: OrderItem;
 };
 
-export type OrderSums = {
-  1: number;
-  2: number;
-  3: number;
-};
+export type OrderSums = { [key: number]: number };
 
 export type Orders = OrdersDataType;
 
 export type AdditionalOrders = OrdersDataType;
+
+type ReportBase = {
+  sum: {
+    muhak: number;
+    hitejinro: number;
+    daesunjujo: number;
+    lotte: number;
+  };
+  goodDay: string;
+  toc: string;
+  daesun: string;
+  gangali: string;
+  daesunEtc: string;
+  chamisul: string;
+  jinro: string;
+  jinrogold: string;
+  sero: string;
+  serosalgu: string;
+  chungha: string;
+};
+type ReportWithTotal = ReportBase & { sum: { total: number } };
+
+export type ReportTablesWithGalmegi = ReportWithTotal & {
+  galmegi: string;
+};
+
+export type ReportTablesWithGalmegi16 = ReportWithTotal & {
+  galmegi19: string;
+  galmegi16: string;
+};
+
+export type ReportPercentagesWithGalmegi = ReportBase & {
+  galmegi: string;
+};
+
+export type ReportPercentagesWithGalmegi16 = ReportBase & {
+  galmegi19: string;
+  galmegi16: string;
+};
+
+export function isReportWithGalmegi16(
+  report:
+    | ReportTablesWithGalmegi
+    | ReportTablesWithGalmegi16
+    | ReportPercentagesWithGalmegi
+    | ReportPercentagesWithGalmegi16
+): report is ReportTablesWithGalmegi16 | ReportPercentagesWithGalmegi16 {
+  return "galmegi19" in report && "galmegi16" in report;
+}

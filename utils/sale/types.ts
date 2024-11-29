c021fd38d0f1099347b7b413e63a06939d2fa2e5
ddolboghi <rhdfyd128@gmail.com>
2024-11-30 03:31:07 +0900

    feat & fix: 제조사별 합계, 갈매기 합계 타입 추가 & 사용하지 않는 타입 제거

--- utils/sale/types.ts
+++ utils/sale/types.ts
@@ -14,51 +14,25 @@ export type Orders = OrdersDataType;
 
 export type OrderSums = { [key: number]: number };
 
-type ReportBase = {
-  sum: {
-    muhak: number;
-    hitejinro: number;
-    daesunjujo: number;
-    lotte: number;
-  };
-  goodDay: string;
-  toc: string;
-  daesun: string;
-  gangali: string;
-  daesunEtc: string;
-  chamisul: string;
-  jinro: string;
-  jinrogold: string;
-  sero: string;
-  serosalgu: string;
-  chungha: string;
-};
-type ReportWithTotal = ReportBase & { sum: { total: number } };
-
-export type ReportTablesWithGalmegi = ReportWithTotal & {
-  galmegi: string;
+export type ProviderSums = {
+  muhak: number;
+  hitejinro: number;
+  daesunjujo: number;
+  lotte: number;
+  total: number;
 };
 
-export type ReportTablesWithGalmegi16 = ReportWithTotal & {
-  galmegi19: string;
-  galmegi16: string;
+export type ProviderPercentages = {
+  muhak: number;
+  hitejinro: number;
+  daesunjujo: number;
+  lotte: number;
 };
 
-export type ReportPercentagesWithGalmegi = ReportBase & {
-  galmegi: string;
+export type GalmegiSums = {
+  original19: number;
+  original16: number;
+  "19": number;
+  "16": number;
+  total: number;
 };
-
-export type ReportPercentagesWithGalmegi16 = ReportBase & {
-  galmegi19: string;
-  galmegi16: string;
-};
-
-export function isReportWithGalmegi16(
-  report:
-    | ReportTablesWithGalmegi
-    | ReportTablesWithGalmegi16
-    | ReportPercentagesWithGalmegi
-    | ReportPercentagesWithGalmegi16
-): report is ReportTablesWithGalmegi16 | ReportPercentagesWithGalmegi16 {
-  return "galmegi19" in report && "galmegi16" in report;
-}

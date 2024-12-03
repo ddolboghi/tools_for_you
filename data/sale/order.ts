c62d70c85106fe658e5c9cb1e58d104d88627eb2
ddolboghi <rhdfyd128@gmail.com>
2024-12-03 15:06:54 +0900

    feat: order 초깃값 갈매기16 추가 버전으로 고정

--- data/sale/order.ts
+++ data/sale/order.ts
@@ -1,5 +1,2 @@
-// {이름, 좋은데이, 톡시리즈, 갈매기}
-export const initOrder = { 0: "", 1: 0, 2: 0, 3: 0 };
-
 // {이름, 좋은데이, 톡시리즈, 갈매기19, 갈매기16}
-export const initOrderWithGamlegi16 = { 0: "", 1: 0, 2: 0, 3: 0, 4: 0 };
+export const initOrder = { 0: "", 1: 0, 2: 0, 3: 0, 4: 0 };

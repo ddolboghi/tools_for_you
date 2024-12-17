329d29b77ecf2f192e1b8d854cbe1a03926e4280
ddolboghi <rhdfyd128@gmail.com>
2024-12-17 16:30:04 +0900

    feat: 판촉물 기본값 추가가

---
+++ utils/sale/promotionStock.ts
@@ -0,0 +1,7 @@
+import { PromotionStock } from "./types";
+
+export const initPromotionStocks: PromotionStock[] = [
+  { name: "헛개밸런스", quantity: undefined },
+  { name: "물티슈", quantity: undefined },
+  { name: "쫀디기", quantity: undefined },
+];

907dc2b3307da321ddc008d015c53edb1e4b27f1
ddolboghi <rhdfyd128@gmail.com>
2024-12-20 02:00:19 +0900

    refactor: 분리한 컴포넌트로 교체체

--- components/sale/Result.tsx
+++ components/sale/Result.tsx
@@ -23,7 +23,9 @@ import {
   getProviderPercentages,
   getProviderSums,
 } from "@/lib/sale/sale";
-import SMGalmegiSummary from "./report/SMGalmegiSummary";
+import SMReport from "./report/SMReport";
+import PromotionStockReport from "./report/PromotionStockReport";
+import OtherCompanyPromotionReport from "./report/OtherCompanyPromotionReport";
 
 type ResultProps = {
   drink: Drink;
@@ -225,87 +227,20 @@ export default function Result({
         {selectedBusinessZone === "수영" && (
           <div>
             <br />
-            <h1>3. 타사 판촉인원 / 판촉물 및 판촉내용</h1>
-            <div>
-              {otherCompanyPromotions.map((promotion, index) => (
-                <p key={`${index}-${promotion.name}`}>
-                  {promotion.name} {promotion.workerNumber || 0}명 /{" "}
-                  {promotion.info}
-                </p>
-              ))}
-            </div>
+            <OtherCompanyPromotionReport
+              otherCompanyPromotions={otherCompanyPromotions}
+            />
             <br />
-            <h1>4. ★자사 판촉물 재고량★ (박스로 기입해서 올려주세요)</h1>
-            <div>
-              {promotionStocks.map((stock, index) => (
-                <p key={`${index}-${stock.name}`}>
-                  &nbsp;- {stock.name} {stock.quantity || 0}박스
-                </p>
-              ))}
-            </div>
+            <PromotionStockReport promotionStocks={promotionStocks} />
           </div>
         )}
         {selectedBusinessZone === "광안" && (
-          <div className="border border-blue-300">
-            <h1>
-              {"<"}이순조SM 퇴근보고{">"}
-            </h1>
-            <section>
-              <h1>1. 야간판촉지역</h1>
-              <p>광안 바닷가</p>
-              <p>총 테이블 수 : {providerSums.total}</p>
-            </section>
-            <section>
-              <h1>2. 야간 음용비</h1>
-              <p>
-                좋은데이 : {reportTables.goodDay}T - {reportPercentages.goodDay}
-                %
-              </p>
-              <p>
-                좋은데이 톡시리즈 : {reportTables.toc}T -{" "}
-                {reportPercentages.toc}%
-              </p>
-              <p>
-                갈매기19 : {reportTables.galmegi19}T -{" "}
-                {reportPercentages.galmegi19}% %
-              </p>
-              <p>
-                갈매기16 : {reportTables.galmegi16}T -{" "}
-                {reportPercentages.galmegi16}%
-              </p>
-              <p>
-                대선 : {reportTables.daesun}T - {reportPercentages.daesun}%
-              </p>
-              <p>
-                강알리 : {reportTables.gangali}T - {reportPercentages.gangali}%
-              </p>
-              <p>
-                진로 : {reportTables.jinro}T - {reportPercentages.jinro}%
-              </p>
-              <p>
-                진로(골드) : {reportTables.jinrogold}T -{" "}
-                {reportPercentages.jinrogold}%
-              </p>
-              <p>
-                참이슬 : {reportTables.chamisul}T - {reportPercentages.chamisul}
-                %
-              </p>
-              <p>C1: T - %</p>
-              <h2>기타</h2>
-              <p>
-                새로: {reportTables.sero}T - {reportPercentages.sero}% %
-              </p>
-              <p>
-                새로(살구): {reportTables.serosalgu}T -{" "}
-                {reportPercentages.serosalgu}%
-              </p>
-              <p>
-                청하: {reportTables.chungha}T - {reportPercentages.chungha}%
-              </p>
-              <br />
-              <SMGalmegiSummary galmegiSums={galmegiSums} />
-            </section>
-          </div>
+          <SMReport
+            providerSums={providerSums}
+            reportTables={reportTables}
+            reportPercentages={reportPercentages}
+            galmegiSums={galmegiSums}
+          />
         )}
         <button
           type="button"

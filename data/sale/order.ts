b08508450f0671487ebf016b82aa662631840e87
ddolboghi <rhdfyd128@gmail.com>
2025-02-17 21:08:20 +0900

    feat: 새 양식 초기값 생성

--- data/sale/order.ts
+++ data/sale/order.ts
@@ -1,2 +1,5 @@
 // {이름, 좋은데이, 톡시리즈, 갈매기19, 갈매기16}
-export const initOrder = { 0: "", 1: 0, 2: 0, 3: 0, 4: 0 };
+export const initOrder1 = { 0: "", 1: 0, 2: 0, 3: 0, 4: 0 };
+
+// {이름, 좋은데이, 부산갈매기, 톡시리즈}
+export const initOrder2 = { 0: "", 1: 0, 2: 0, 3: 0 };

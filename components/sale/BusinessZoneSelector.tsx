287717c128b9207f6fae36ad8c4a798b2e7f6ee0
ddolboghi <rhdfyd128@gmail.com>
2025-03-20 19:23:02 +0900

    fix: select 대신 스크롤 가능하도록 수정

--- components/sale/BusinessZoneSelector.tsx
+++ components/sale/BusinessZoneSelector.tsx
@@ -1,29 +1,51 @@
 import { businessZones } from "@/utils/sale/businessZones";
+import { ChevronDown } from "lucide-react";
 
 type BusinessZoneSelectorProps = {
   selectedBusinessZone: string;
   handleSelectBusinessZone: (businessZone: string) => void;
 };
 
+import { useState } from "react";
+
 export default function BusinessZoneSelector({
   selectedBusinessZone,
   handleSelectBusinessZone,
 }: BusinessZoneSelectorProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleSelect = (businessZoneName: string) => {
+    handleSelectBusinessZone(businessZoneName);
+    setIsOpen(false);
+  }
+
   return (
-    <select
-      value={selectedBusinessZone}
-      onChange={(e) => handleSelectBusinessZone(e.target.value)}
-      className="text-sm border border-gray-300 rounded p-1 text-black"
-    >
-      {businessZones.map((businessZone) => (
-        <option
-          key={businessZone.id}
-          value={businessZone.name}
-          className="text-sm"
-        >
-          {businessZone.name}
-        </option>
-      ))}
-    </select>
+    <div className="relative w-64">
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="flex justify-between items-center text-sm border border-gray-300 rounded p-1 text-black w-1/2 text-left"
+      >
+        {selectedBusinessZone || "상권을 선택해주세요."}
+        <ChevronDown
+          className={`transition-transform duration-200 ${
+            isOpen ? "rotate-180" : "rotate-0"
+          }`}
+        />
+      </button>
+      {isOpen && (
+        <ul className="absolute w-1/2 border border-gray-300 bg-white max-h-72 overflow-y-auto rounded shadow-md mt-1">
+          {businessZones.map((businessZone) => (
+            <li
+              key={businessZone.id}
+              onClick={() => handleSelect(businessZone.name)}
+              className="p-2 text-sm hover:bg-gray-200 cursor-pointer"
+            >
+              {businessZone.name}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 }
+

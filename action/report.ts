ada48fd15c9de51a56556abc69aaedfc69911a64
ddolboghi <rhdfyd128@gmail.com>
2025-04-08 20:25:42 +0900

    feat: 계산 결과 및 전환/추가 합계 저장 API 추가

---
+++ action/report.ts
@@ -0,0 +1,34 @@
+"use server";
+
+import { supabaseClient } from "@/lib/getSupabaseClient";
+import { BskyReport, OrderSums } from "@/utils/sale/types";
+
+export const insertReport = async (
+  businessZone: string,
+  visitShopNumber: number,
+  calculationResult: BskyReport,
+  orderSums: OrderSums,
+  additionalOrderSums: OrderSums
+) => {
+  try {
+    const { error } = await supabaseClient
+      .from("report")
+      .insert([
+        {
+          business_zone: businessZone,
+          visit_shop_number: visitShopNumber,
+          calculation_result: calculationResult,
+          order_sums: orderSums,
+          additional_order_sums: additionalOrderSums,
+        },
+      ])
+      .select();
+
+    if (error) {
+      throw error;
+    }
+    return true;
+  } catch (e) {
+    return false;
+  }
+};

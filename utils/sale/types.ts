2d74f5654d58c59121cbe6c6e3f40f9db7291f88
ddolboghi <rhdfyd128@gmail.com>
2025-04-08 20:27:11 +0900

    fix: 사용하지 않는 타입 제거

--- utils/sale/types.ts
+++ utils/sale/types.ts
@@ -1,11 +1,3 @@
-export type Drink = {
-  [key: string]: { [key: number]: number };
-};
-
-export type Percentages = {
-  [key: string]: { [key: number]: number };
-};
-
 type OrdersDataType = {
   [key: number]: { [key: number]: number | string };
 };
@@ -13,21 +5,6 @@ type OrdersDataType = {
 export type Orders = OrdersDataType;
 
 export type OrderSums = { [key: string]: number };
-
-export type ProviderSums = {
-  muhak: number;
-  hitejinro: number;
-  daesunjujo: number;
-  lotte: number;
-  total: number;
-};
-
-export type ProviderPercentages = {
-  muhak: number;
-  hitejinro: number;
-  daesunjujo: number;
-  lotte: number;
-};
 
 export type GalmegiSums = {
   original19: number;
@@ -46,38 +23,6 @@ export type OtherCompanyPromotionResult = {
 export type PromotionStock = {
   name: string;
   quantity: number | undefined;
-};
-
-export type ReportTables = {
-  goodDay: number;
-  toc: number;
-  galmegi19: number;
-  galmegi16: number;
-  daesun: number;
-  gangali: number;
-  daesunEtc: number;
-  chamisul: number;
-  jinro: number;
-  jinrogold: number;
-  sero: number;
-  serosalgu: number;
-  chungha: number;
-};
-
-export type ReportPercentages = {
-  goodDay: string | number;
-  toc: string | number;
-  galmegi19: string | number;
-  galmegi16: string | number;
-  daesun: string | number;
-  gangali: string | number;
-  daesunEtc: string | number;
-  chamisul: string | number;
-  jinro: string | number;
-  jinrogold: string | number;
-  sero: string | number;
-  serosalgu: string | number;
-  chungha: string | number;
 };
 
 type ReportResult = {

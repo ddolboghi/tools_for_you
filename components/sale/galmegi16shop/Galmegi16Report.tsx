d73f49c3cd367119b4e00b726c208c11e1b792db
ddolboghi <rhdfyd128@gmail.com>
2025-04-08 20:26:56 +0900

    fix: eslint 경고 무시

--- components/sale/galmegi16shop/Galmegi16Report.tsx
+++ components/sale/galmegi16shop/Galmegi16Report.tsx
@@ -28,6 +28,7 @@ export default function Galmegi16Report({
 
   useEffect(() => {
     fetchShopList();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [businessZone]);
 
   const handleGalmegi16ShopReportClipboard = () => {
